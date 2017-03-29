819cc6b081a6be94204f626a715d23780730ba1a
Prasanna <prasannamishra50@gmail.com>
2017-03-29 15:37:05 +0545

    initial commit

---
+++ src/components/ludo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Board from './board';
+
+class Ludo extends Component {
+  render() {
+    return (
+      <div>
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default Ludo;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Ludo from './components/ludo';
+
+import './style.css';
+
+ReactDom.render(
+  <Ludo />, document.getElementById('root')
+);
